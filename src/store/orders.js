f0c9955c88d09bfe5e15d60d01ccce1a2134e5ff
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2023-04-17 15:50:31 -0300

    Add order details

--- src/store/orders.js
+++ src/store/orders.js
@@ -1,24 +1,34 @@
 import axios from 'axios';
 
 const state = {
+    baseUrl: 'http://localhost:4000/api/v1/deliveries/',
     orders: [],
+    order: [],
     totalResults: null,
 };
 
 const actions = {
     async fetchOrders({ commit }, payload) {
-        const response = await axios.get('http://localhost:4000/api/v1/deliveries/', 
+        const response = await axios.get(state.baseUrl, 
         {
             params: payload
         });
         commit('setTotalResults', response.data.data.totalResults)
         commit('setOrders', response.data.data.deliveries)
     },
+    async fetchOrder({ commit }, id) {
+        const response = await axios.get(state.baseUrl + id);
+        console.log(response.data.data)
+        commit('setOrder', response.data.data)
+    },
 };
 
 const mutations = {
     setOrders(state, orders) {
         state.orders = orders;
+    },
+    setOrder(state, order) {
+        state.order = order;
     },
     setTotalResults(state, totalResults) {
         state.totalResults = totalResults;
--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,16 +1,63 @@
 <template>
-    <div>
-        DETALLE
+    <div class="row justify-content-center mt-5">
+        <div class="col-8">
+            <Title class="text-center" :title="title" />
+            <div class="d-flex justify-content-center mt-5">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Origin</h5>
+                        <div>
+                            <b class="text-secondary">City:&nbsp;</b>
+                            <label>{{ order?.origin?.city}}</label>
+                        </div>
+                        <div>
+                            <b class="text-secondary mr-2">Street:&nbsp;</b>
+                            <label>{{ order?.origin?.street}}, {{ order.origin?.number }}</label>
+                        </div>
+                        <div>
+                            <b class="text-secondary">Postal code:&nbsp;</b>
+                            <label>{{ order?.origin?.postalCode }}</label>
+                        </div>
+                        <h5 class="card-title mt-3">Destination</h5>
+                        <div>
+                            <b class="text-secondary">City:&nbsp;</b>
+                            <label>{{ order?.destination?.city}}</label>
+                        </div>
+                        <div>
+                            <b class="text-secondary">Street:&nbsp;</b>
+                            <label>{{ order?.destination?.street}}, {{ order.destination?.number }}</label>
+                        </div>
+                        <div>
+                            <b class="text-secondary">Postal code:&nbsp;</b>
+                            <label>{{ order?.destination?.postalCode }}</label>
+                        </div>
+                        <h5 class="card-title mt-3">Products</h5>
+                        <div v-for="(product, index) in order.products" :key="index">
+                            <div>{{ product }}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default{
     data(){
-        return{}
+        return{
+            title: null,
+        }
     },
     mounted(){
         const id = this.$route.query.orderId;
+        this.title = `Detalle #${id}`
+        this.$store.dispatch('orders/fetchOrder', id);
+    },
+    computed:{
+        ...mapState('orders', ['order'])
     }
 }
 </script>
